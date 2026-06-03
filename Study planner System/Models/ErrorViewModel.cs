b58bad26c2c3e20f11f517239a3c7ee2775932d4
alexander22042009 <101943388+alexander22042009@users.noreply.github.com>
2026-06-03 08:50:21 +0300

    Add project files.

---
+++ Study planner System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Study_planner_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
